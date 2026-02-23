002aa8a3f21382964122f024dc3bb3f58582f321
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-02-23 11:39:58 +0100

    cifrado basico

---
+++ MvcCoreUtilidades/Controllers/CifradosController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class CifradosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CifradoBasico()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CifradoBasico(string contenido, string resultado, string accion)
+        {
+            //CIFRAMOS EL CONTENIDO
+            string response = HelperCryptography.EncriptarTextoBasico(contenido);
+
+            if (accion.ToLower() == "cifrar")
+            {
+                ViewData["TEXTOCIFRADO"] = response;
+            }else if(accion.ToLower() == "comparar")
+            {
+                //SI EL USUARIO QUIERE COMPARAR, NOS ESTARÁ ENVIANDO
+                //EL TEXTO A COMPARAR EN resultado
+                if(response != resultado)
+                {
+                    ViewData["MENSAJE"] = "Los datos no coinciden";
+                }
+                else
+                {
+                    ViewData["MENSAJE"] = "Contenidos iguales :)";
+                }
+            }
+            return View();
+        }
+    }
+}
